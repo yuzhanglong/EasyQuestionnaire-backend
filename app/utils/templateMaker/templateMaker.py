dbddaec80107f3ef7bcadf2d95af66e7557ffd95
yuzhanglong <yuzl1123@163.com>
2020-02-03 16:26:38 +0800

    优化问卷模板爬虫逻辑 防止重复爬取相同的数据

--- app/utils/templateMaker/templateMaker.py
+++ app/utils/templateMaker/templateMaker.py
@@ -1,5 +1,6 @@
 from app.utils.templateMaker.spiders.wenjuanwang import makeQuestionnaireStructFromWJW, getLinks
 from app.models.questionnaire import Questionnaire
+from app.models.basicInfo import BasicInfo
 import time
 from app.utils.emailtools import sendEmail
 from app.extensions import db
@@ -7,15 +8,46 @@ from flask import current_app
 import app.config as conf
 
 
+class SpiderTag:
+    def __init__(self, index):
+        self.info = BasicInfo.objects.first()
+        if not self.info:
+            self.info = self.initDocument()
+        self.index = index
+
+    def getNewestLink(self):
+        link = self.info.spidersTagLinks
+        return link[self.index]
+
+    def renewNewestLink(self, newLink):
+        self.info.spidersTagLinks[self.index] = newLink
+        self.info.save()
+
+    @staticmethod
+    def initDocument():
+        basicinfo = BasicInfo()
+        basicinfo.save()
+        return basicinfo
+
+
 def pushWJWDataToDB():
     with db.app.app_context():
         myList = getLinks(2)
-        count = 1
+        # 计数器
+        count = 0
         failedNum = 0
         successNum = 0
         # 防止时间戳重复
         startTick = int(round(time.time() * 1000))
+        form = SpiderTag(0)
+        # 获得当前网站下已经爬到的最新链接
+        newestLink = form.getNewestLink()
         for l in myList:
+            count += 1
+            # 发现当前链接和最新链接相等
+            if l == newestLink:
+                sendSuccessEmail(successNum, failedNum)
+                return
             nowTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             # 防止由于网站原因拿不到部分数据 如果异常则加载下一份问卷
             try:
@@ -26,14 +58,15 @@ def pushWJWDataToDB():
                 newQuestionnaire = Questionnaire(questionnaireUserId="templateMaker", questionnaireFlag=tick,
                                                  questionnaireBasicData=p,
                                                  questionnaireRenewTime=nowTime)
+                successNum += 1
             except:
                 current_app.logger.info("第" + str(count) + "份问卷模板获取失败.....")
                 failedNum += 1
-                count += 1
                 continue
             current_app.logger.info("第" + str(count) + "份问卷模板获取成功.....")
-            successNum += 1
-            count += 1
+            # 最新数据 并且生效写入数据库了
+            if successNum == 1:
+                form.renewNewestLink(l)
             newQuestionnaire.save()
         sendSuccessEmail(successNum, failedNum)
 
